0164454015a2cbdec05428f45312dabb8e7f28cf
Ricardo Avelino <rick58@ymail.com>
2021-06-09 19:29:49 -0300

    Desafio Nº 2 do HackerRank da Cubos Academy

---
+++ me_segue_no_insta.js
@@ -0,0 +1,15 @@
+function solucao(largura, altura) {
+  //seu codigo aqui
+    if (altura > largura) {
+        console.log ("RETRATO")
+
+}else if (altura < largura) {
+
+        console.log ("PAISAGEM")
+}else if (altura == largura){
+
+        console.log ("QUADRADA")
+}
+
+  
+}

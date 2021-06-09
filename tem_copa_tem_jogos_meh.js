c0fd3ec0ad513fb112d6267cc88830d4f042ccb5
Ricardo Avelino <rick58@ymail.com>
2021-06-09 19:32:32 -0300

    Desafio Nº 7 do HackerRank da Cubos Academy

---
+++ tem_copa_tem_jogos_meh.js
@@ -0,0 +1,12 @@
+function solucao(ano) {
+	//seu codigo aqui
+    if (ano % 4 == 2) {
+    console.log("COPA")
+}
+else if (ano % 4 == 0) {
+    console.log("JOGOS")
+}
+else {
+    console.log("MEH")
+}
+}
